d676bac31abc13e26df37fe621d1e6a8ba853bee
pkl0912 <pklpkl0912@gmail.com>
2024-08-22 16:40:25 +0900

    first commit

---
+++ src/main/java/com/example/letscareer/LetscareerApplication.java
@@ -0,0 +1,13 @@
+package com.example.letscareer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetscareerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetscareerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/letscareer/LetscareerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.letscareer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetscareerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
