526c740d4ec35f7b0fbacf5eed60954b5f21e386
oosedus <seayeon0729@gmail.com>
2024-09-01 17:53:28 +0900

    [fix] schedule progress 네이밍 수정

--- src/main/java/com/example/letscareer/schedule/domain/Progress.java
+++ src/main/java/com/example/letscareer/schedule/domain/Progress.java
@@ -6,6 +6,6 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum Progress {
-    PASS("최종 합격"), FAIL("최종 불합격"), DO("진행중");
+    PASS("최종 합격"), FAIL("최종 불합격"), DO("공고 진행중");
     private final String value;
 }
