2b6383142fc69f1b60cf03e86aab4da31a4c38c1
pkl0912 <pklpkl0912@gmail.com>
2024-09-02 20:06:45 +0900

    #43 [feat] 중간회고 service 추가

--- src/main/java/com/example/letscareer/common/exception/enums/ErrorCode.java
+++ src/main/java/com/example/letscareer/common/exception/enums/ErrorCode.java
@@ -18,7 +18,7 @@ public enum ErrorCode {
     //404
     NOT_FOUND_RESOURCE_EXCEPTION(HttpStatus.NOT_FOUND, "해당 자원을 찾을 수 없습니다."),
     USER_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "해당 유저는 존재하지 않습니다."),
-    KEYWORD_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "해당 키워드는 존재하지 않습니다."),
+    MID_REVIEW_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "해당 중간회고는 존재하지 않습니다."),
     CAREER_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "해당 커리어는 존재하지 않습니다."),
     TODO_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "해당 투두는 존재하지 않습니다"),
     SCHEDULE_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "해당 ID의 지원 일정을 찾을 수 없습니다."),
