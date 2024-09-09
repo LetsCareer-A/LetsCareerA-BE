dc236ea01c1277e6aef82a900e1e9837d2be4fb5
pkl0912 <pklpkl0912@gmail.com>
2024-09-09 18:31:04 +0900

    #84 [refactor] 회고 보드 api 수정

--- src/main/java/com/example/letscareer/schedule/domain/dto/CompanyReviewDTO.java
+++ src/main/java/com/example/letscareer/schedule/domain/dto/CompanyReviewDTO.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 public record CompanyReviewDTO(
         String company,
+        String department,
+
         List<CompanyReviewDetailDTO> interviewReviews,  // 면접 리뷰
         List<CompanyReviewDetailDTO> midtermReviews  // 중간 리뷰
 ) {
