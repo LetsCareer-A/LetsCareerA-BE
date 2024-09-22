e26a048019ca219c2e1fc19334f32cfc3b25f014
oosedus <seayeon0729@gmail.com>
2024-09-22 15:30:14 +0900

    [refactor] 전형 단계 상태 추가

--- src/main/java/com/example/letscareer/stage/domain/model/Status.java
+++ src/main/java/com/example/letscareer/stage/domain/model/Status.java
@@ -6,6 +6,6 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum Status {
-    DO("준비 진행 중"), FIN ("진행 완료");
+    DO("준비 진행 중"), FIN ("진행 완료"), FAIL("불합경"), PASS("합격");
     private final String value;
 }
